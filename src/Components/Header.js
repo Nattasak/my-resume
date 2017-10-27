ad6f652cbbb2f246105318c2c9549e1d322d67f8
Nattasak <dottedes@gmail.com>
2017-10-27 22:05:27 +0700

    css responsive header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,10 +5,7 @@ class Header extends Component {
   render() {
     return (
       <header className="header section">
-        <h1 className="section-title">
-          <span className="first-name">Nattasak</span>
-          <span className="last-name">Lertpoompunya</span>
-        </h1>
+        <h1 className="section-title">Nattasak Lertpoompunya</h1>
         <nav className="main-nav">
           <div className="container">
             <ul className="main-nav-items">
