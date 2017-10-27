0836bc09a6f1312e6d0ce44a8eda12030c5031d0
Nattasak <dottedes@gmail.com>
2017-10-27 15:00:26 +0700

    add some text to app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        App change
+        App change AGAIN !!
         <Footer />
       </div>
     );
