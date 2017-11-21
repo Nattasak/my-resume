e230bf6e24fd8271c485777a3b91c699960de3e1
Nattasak <dottedes@gmail.com>
2017-11-21 15:59:59 +0700

    improve styles

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,16 +6,14 @@ export default class About extends Component {
     return (
       <section className="about-section section">
         <div className="container">
-          <h3 className="section-title">About</h3>
           <div className="flex-container">
             <div className="flex-item">
               <div className="bio">
-                Hi' my name is Nat, I'm 27 years old living in Bangkok, Thailand.<br />
+                Hi' my name is Nat, I'm 27 years old living in Bangkok, Thailand. 
                 I love to learn new things. I wanna make the world better.
               </div>
               <ul className="about-items">
                 <li className="about-item">
-                  <i className="fa fa-eercast fa-2x" aria-hidden="true"></i>
                   <span className="title">Front End</span><span>  -  BizIdea Digital Marketing (1 yr)</span>
                 </li>
               </ul>
