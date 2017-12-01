911d6322ed72afb56a11ff8181f9f9d5bc984573
Nattasak <dottedes@gmail.com>
2017-12-01 18:24:21 +0700

    update const name

--- src/Components/Knowledge.js
+++ src/Components/Knowledge.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import '../Styles/Skills.css'
 
-const KnowledgeList = ({ title, url='' }) => (
+const List = ({ title, url='' }) => (
   <li className="skills-item">
     <i className="fa fa-linode fa-2x" aria-hidden="true"></i>
     <span>
@@ -19,12 +19,12 @@ class Knowledge extends Component {
           <div className="flex-container">
             <div className="flex-item">
               <ul className="skills-items">
-                <KnowledgeList title='HTTP' />
-                <KnowledgeList title='DOM handling' />
-                <KnowledgeList title='AJAX / RESTful API' url='https://www.youtube.com/watch?v=s7wmiS2mSXY' />
-                <KnowledgeList title='How JavaScript works' url='https://www.youtube.com/watch?v=8aGhZQkoFbQ' />
-                <KnowledgeList title='Functional Programming' url='https://www.youtube.com/watch?v=FYXpOjwYzcs' />
-                <KnowledgeList title='JS Style Guide' url='https://github.com/airbnb/javascript' />
+                <List title='HTTP' />
+                <List title='DOM handling' />
+                <List title='AJAX / RESTful API' url='https://www.youtube.com/watch?v=s7wmiS2mSXY' />
+                <List title='How JavaScript works' url='https://www.youtube.com/watch?v=8aGhZQkoFbQ' />
+                <List title='Functional Programming' url='https://www.youtube.com/watch?v=FYXpOjwYzcs' />
+                <List title='JS Style Guide' url='https://github.com/airbnb/javascript' />
               </ul>
             </div>
           </div>
