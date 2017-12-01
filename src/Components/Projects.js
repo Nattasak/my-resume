1de6ccb69472ff9cbb1501230087ea2f11c6df4c
Nattasak <dottedes@gmail.com>
2017-12-01 18:36:10 +0700

    refactoring desc to children

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react'
 import '../Styles/Projects.css'
 
+const Project = ({ title, ghpage, children }) => (
+  <li className="projects-item">
+    <i className="fa fa-grav fa-2x" aria-hidden="true"></i>
+    <a href={`https://nattasak.github.io/${ghpage}`} target="_blank" rel="noopener noreferrer">{title}</a>
+    <p>{children}</p>
+  </li>
+)
+
 class Projects extends Component {
   render() {
     return (
       <section className="projects-section section">
         <div className="container">
           <h3 className="section-title">Projects</h3>
-
           <div className="flex-container">
             <div className="flex-item">
               <ul className="projects-items">
-                <li className="projects-item">
-                  <i className="fa fa-grav fa-2x" aria-hidden="true"></i>
-                  <a href="https://nattasak.github.io/anime-splash/" target="_blank" rel="noopener noreferrer">Anime Splash</a><br />
-                  <p>Angular 5 + Bootstrap + HttpClient</p>
-                </li>
-                <li className="projects-item">
-                  <i className="fa fa-grav fa-2x" aria-hidden="true"></i>
-                  <a href="https://nattasak.github.io/bulma-axios/" target="_blank" rel="noopener noreferrer">Bulma Axios</a><br />
-                  <p>React 16 + React router + Bulma + Axios</p>
-                </li>
-                <li className="projects-item">
-                  <i className="fa fa-grav fa-2x" aria-hidden="true"></i>
-                  <a href="https://nattasak.github.io/job-quest-winter-2017/" target="_blank" rel="noopener noreferrer">Todo Winter</a><br />
-                  <p>This project is a part of TakeMeTour's Job Quest (Winter 2017 Edition)</p>
-                </li>
+              <Project title='Anime Splash' ghpage='anime-splash'>
+                Angular 5 + Bootstrap + HttpClient
+              </Project>
+              <Project title='Bulma Axios' ghpage='bulma-axios'>
+                React 16 + React router + Bulma + Axios
+              </Project>
+              <Project title='Todo Winter' ghpage='job-quest-winter-2017'>
+                This project is a part of TakeMeTour's Job Quest
+              </Project>
               </ul>
             </div>
           </div>
-
         </div>
       </section>
     )
