90d65c5f2e60a26bce571a5384c6e840b25aef89
Nattasak <dottedes@gmail.com>
2017-12-03 02:14:05 +0700

    responsive header css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import './Styles/App.css'
 
 // Components
-import Header from './Components/Header.js'
-import About from './Components/About.js'
-import Projects from './Components/Projects.js'
-import JSPlayground from './Components/JSPlayground.js'
-import Skills from './Components/Skills.js'
-import Knowledge from './Components/Knowledge.js'
+import Header from './Components/Header'
+import About from './Components/About'
+import Projects from './Components/Projects'
+import JSPlayground from './Components/JSPlayground'
+import Skills from './Components/Skills'
+import Knowledge from './Components/Knowledge'
 
 class App extends Component {
   constructor(props) {
