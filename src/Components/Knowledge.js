2c0cf01a0f81373a237bc293b21ff9b5ee23129c
Nattasak <dottedes@gmail.com>
2017-12-02 18:39:20 +0700

    update link in knwoledge section

--- src/Components/Knowledge.js
+++ src/Components/Knowledge.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import '../Styles/Skills.css'
 
-const List = ({ title, url='' }) => (
-  <li className="skills-item">
-    <i className="fa fa-linode fa-2x" aria-hidden="true"></i>
-    <span>
-      <a href={url} target="_blank" rel="noopener noreferrer">{title}</a>
-    </span>
-  </li>
-)
+const List = (props) => {
+  if (props.author) {
+    var author = '  -  ' + props.author
+  }
+
+  return (
+    <li className="skills-item">
+      <i className="fa fa-linode fa-2x" aria-hidden="true"></i>
+      <span>
+        <a href={props.children} target="_blank" rel="noopener noreferrer">{props.title}</a>
+        {author}
+      </span>
+    </li>
+  )
+}
 
 class Knowledge extends Component {
   render() {
@@ -21,10 +28,24 @@ class Knowledge extends Component {
               <ul className="skills-items">
                 <List title='HTTP' />
                 <List title='DOM handling' />
-                <List title='AJAX / RESTful API' url='https://www.youtube.com/watch?v=s7wmiS2mSXY' />
-                <List title='How JavaScript works' url='https://www.youtube.com/watch?v=8aGhZQkoFbQ' />
-                <List title='Functional Programming' url='https://www.youtube.com/watch?v=FYXpOjwYzcs' />
-                <List title='JS Style Guide' url='https://github.com/airbnb/javascript' />
+                <List title='React: Presentational and Container Components' author='Dan Abramov'>
+                  https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
+                </List>
+                <List title='React: Clean Code vs. Dirty Code' author='Donavon West'>
+                  http://americanexpress.io/clean-code-dirty-code/
+                </List>
+                <List title='JS: What the heck is the event loop anyway?' author='Philip Roberts'>
+                  https://www.youtube.com/watch?v=8aGhZQkoFbQ
+                </List>
+                <List title='JS: Airbnb JavaScript Style Guide' author='Airbnb'>
+                  https://github.com/airbnb/javascript
+                </List>
+                <List title='JS: Functional Programming Basics In ES6' author='Jeremy Fairbank'>
+                  https://www.youtube.com/watch?v=FYXpOjwYzcs
+                </List>
+                <List title='What is an API?' author='MuleSoft'>
+                  https://www.youtube.com/watch?v=s7wmiS2mSXY
+                </List>
               </ul>
             </div>
           </div>
