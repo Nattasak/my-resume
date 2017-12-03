646fb18b7569958b225f46fb0256f2eba7a2135c
Nattasak <dottedes@gmail.com>
2017-12-03 19:09:51 +0700

    change the way to pull data Presentational component to JSON

--- src/Components/Knowledge.js
+++ src/Components/Knowledge.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react'
 import '../Styles/Skills.css'
 
-const List = (props) => {
-  if (props.author) {
-    var author = '  -  ' + props.author
-  }
-
-  var link
-  
-  if (props.children) {
-    link = <a href={props.children} target="_blank" rel="noopener noreferrer">{props.title}</a>
-  } else {
-    link = props.title
-  }
-
-  return (
-    <li className="skills-item">
-      <i className="fa fa-search-plus fa-2x" aria-hidden="true"></i>
-      <span>{link}{author}</span>
-    </li>
-  )
-}
-
 class Knowledge extends Component {
   render() {
     return (
@@ -31,26 +10,17 @@ class Knowledge extends Component {
           <div className="flex-container">
             <div className="flex-item">
               <ul className="skills-items">
-                <List title='HTTP' />
-                <List title='DOM handling' />
-                <List title='React: Presentational and Container Components' author='Dan Abramov'>
-                  https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
-                </List>
-                <List title='React: Clean Code vs. Dirty Code' author='Donavon West'>
-                  http://americanexpress.io/clean-code-dirty-code/
-                </List>
-                <List title='JS: What the heck is the event loop anyway?' author='Philip Roberts'>
-                  https://www.youtube.com/watch?v=8aGhZQkoFbQ
-                </List>
-                <List title='JS: Airbnb JavaScript Style Guide' author='Airbnb'>
-                  https://github.com/airbnb/javascript
-                </List>
-                <List title='JS: Functional Programming Basics In ES6' author='Jeremy Fairbank'>
-                  https://www.youtube.com/watch?v=FYXpOjwYzcs
-                </List>
-                <List title='What is an API?' author='MuleSoft'>
-                  https://www.youtube.com/watch?v=s7wmiS2mSXY
-                </List>
+                { KNOWLEDGES.map(knowledge => 
+                  <li className="skills-item" key={knowledge.id}>
+                    <i className="fa fa-search-plus fa-2x" aria-hidden="true"></i>
+                    <span>
+                      { knowledge.url ?
+                        <a href={knowledge.url} target="_blank" rel="noopener noreferrer">{knowledge.title}</a>
+                        : knowledge.title }
+                      { knowledge.author ? `${'  -  '}${knowledge.author}` : null }
+                    </span>
+                  </li>
+                )}
               </ul>
             </div>
           </div>
@@ -59,5 +29,56 @@ class Knowledge extends Component {
     )
   }
 }
+
+const KNOWLEDGES = [
+  {
+    id: 1,
+    title: 'HTTP',
+    author: '',
+    url: ''
+  },
+  {
+    id: 2,
+    title: 'DOM handling',
+    author: '',
+    url: ''
+  },
+  {
+    id: 3,
+    title: 'React: Presentational and Container Components',
+    author: 'Dan Abramov',
+    url: 'https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0'
+  },
+  {
+    id: 4,
+    title: 'React: Clean Code vs. Dirty Code',
+    author: 'Donavon West',
+    url: 'http://americanexpress.io/clean-code-dirty-code'
+  },
+  {
+    id: 5,
+    title: 'JS: What the heck is the event loop anyway?',
+    author: 'Philip Roberts',
+    url: 'https://www.youtube.com/watch?v=8aGhZQkoFbQ'
+  },
+  {
+    id: 6,
+    title: 'JS: Airbnb JavaScript Style Guide',
+    author: 'Airbnb',
+    url: 'https://github.com/airbnb/javascript'
+  },
+  {
+    id: 7,
+    title: 'JS: Functional Programming Basics In ES6',
+    author: 'Jeremy Fairbank',
+    url: 'https://www.youtube.com/watch?v=FYXpOjwYzcs'
+  },
+  {
+    id: 8,
+    title: 'What is an API?',
+    author: 'MuleSoft',
+    url: 'https://www.youtube.com/watch?v=s7wmiS2mSXY'
+  }
+]
 
 export default Knowledge
