62af9d320f7ff5fb69f92a53278e5be4a113e315
Nattasak <dottedes@gmail.com>
2017-12-09 00:56:45 +0700

    change js-data to json-data

--- src/Components/Knowledge.js
+++ src/Components/Knowledge.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../Styles/Skill.css'
 
-const title = 'Essential knowledge'
+import KNOWLEDGE from '../data/knowledge.json';
 
 const ListItem = (props) => {
   return (
@@ -17,7 +17,7 @@ const ListItem = (props) => {
   )
 }
 
-const KnowledgeList = (props) => {
+const List = (props) => {
   const data = props.data
   const listItems = data.map((item) => 
     <ListItem 
@@ -37,66 +37,15 @@ const Knowledge = () => {
   return (
     <section className="skills-section section">
       <div className="container">
-        <h3 className="section-title">{title}</h3>
+        <h3 className="section-title">Essential knowledge</h3>
         <div className="flex-container">
           <div className="flex-item">
-            <KnowledgeList data={KNOWLEDGES} />
+            <List data={KNOWLEDGE} />
           </div>
         </div>
       </div>
     </section>
   )
 }
-
-const KNOWLEDGES = [
-  {
-    id: 1,
-    title: 'HTTP',
-    author: '',
-    url: ''
-  },
-  {
-    id: 2,
-    title: 'DOM handling',
-    author: '',
-    url: ''
-  },
-  {
-    id: 3,
-    title: 'React: Presentational and Container Components',
-    author: 'Dan Abramov',
-    url: 'https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0'
-  },
-  {
-    id: 4,
-    title: 'React: Clean Code vs. Dirty Code',
-    author: 'Donavon West',
-    url: 'http://americanexpress.io/clean-code-dirty-code'
-  },
-  {
-    id: 5,
-    title: 'JS: What the heck is the event loop anyway?',
-    author: 'Philip Roberts',
-    url: 'https://www.youtube.com/watch?v=8aGhZQkoFbQ'
-  },
-  {
-    id: 6,
-    title: 'JS: Airbnb JavaScript Style Guide',
-    author: 'Airbnb',
-    url: 'https://github.com/airbnb/javascript'
-  },
-  {
-    id: 7,
-    title: 'JS: Functional Programming Basics In ES6',
-    author: 'Jeremy Fairbank',
-    url: 'https://www.youtube.com/watch?v=FYXpOjwYzcs'
-  },
-  {
-    id: 8,
-    title: 'What is an API?',
-    author: 'MuleSoft',
-    url: 'https://www.youtube.com/watch?v=s7wmiS2mSXY'
-  }
-]
 
 export default Knowledge
