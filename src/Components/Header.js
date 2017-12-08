4231bbe35a12f0a882340c35e5b0b1a11eee1452
Nattasak <dottedes@gmail.com>
2017-12-09 01:55:44 +0700

    change js-data to json-data Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,33 +4,29 @@ import SayHi from './SayHi.js'
 
 const myName = 'Nattasak Lertpoompunya'
 
-const HeaderIconList = (props) => {
-  return (
-    <li className="main-nav-item">
-      <a href={props.url} target="_blank" rel="noopener noreferrer">
-        <i className={`fa fa-${props.icon} fa-3x`} aria-hidden="true"></i>
-      </a>
-    </li>
-  )
-}
+const List = (props) => (
+  <li className="main-nav-item">
+    <a href={props.url} target="_blank" rel="noopener noreferrer">
+      <i className={`fa fa-${props.icon} fa-3x`} aria-hidden="true"></i>
+    </a>
+  </li>
+)
 
-const Header = () => {
-  return (
-    <header className="header section">
-      <h1 className="section-title">{myName}</h1>
-      <nav className="main-nav">
-        <div className="container">
-          <ul className="main-nav-items">
-            <li className="main-nav-item say-hello">
-              <SayHi />
-            </li>
-            <HeaderIconList url='https://github.com/Nattasak' icon='github' />
-            <HeaderIconList url='https://medium.com/@nattasak111' icon='medium' />
-          </ul>
-        </div>
-      </nav>
-    </header>
-  )
-}
+const Header = () => (
+  <header className="header section">
+    <h1 className="section-title">{myName}</h1>
+    <nav className="main-nav">
+      <div className="container">
+        <ul className="main-nav-items">
+          <li className="main-nav-item say-hello">
+            <SayHi />
+          </li>
+          <List url='https://github.com/Nattasak' icon='github' />
+          <List url='https://medium.com/@nattasak111' icon='medium' />
+        </ul>
+      </div>
+    </nav>
+  </header>
+)
 
 export default Header
